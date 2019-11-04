1c1d500a9ac6d86496827b97effbe0958cb29980
Meghan Adkins <meghandadkins@gmail.com>
2019-11-04 10:31:36 -0500

    inital commit

---
+++ src/components/Boulder.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledBoulder = styled.div``;
+
+function Boulder() {
+  return (
+    <StyledBoulder>
+        
+    </StyledBoulder>
+  )
+}
+
+export default Boulder
